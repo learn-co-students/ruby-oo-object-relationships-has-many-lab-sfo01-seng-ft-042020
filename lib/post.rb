094dba2f4b528fabd1699425fa39a9f0038300bf
Guosa_Edebiri <gedebiri@gmail.com>
2020-04-30 09:13:09 -0700

    Done.

---
+++ lib/post.rb
@@ -0,0 +1,30 @@
+
+class Post
+
+    attr_accessor :title, :author
+
+    @@all = []
+
+    def initialize(title, author = nil)
+        @title = title
+        @author = author
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def author_name
+
+        if self.author
+            self.author.name
+        elsif
+            nil
+        end
+
+
+    end
+
+
+end
